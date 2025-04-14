330e5ed1b5d35ad5bcf950cf19c922da02bb1bcf
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-04-14 13:34:16 -0300

    :sparkles: feat: create component/section Projetos

---
+++ src/components/Projetos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projetos() {
+  return (
+    <section id="projetos" className="section">
+      <h2>Projetos</h2>
+      <p>Aqui vão alguns dos projetos que desenvolvi com React e outras tecnologias...</p>
+    </section>
+  );
+}
+
+export default Projetos;
