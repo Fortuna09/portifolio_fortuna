22780df2536e2f93ecc857b9f9a23743cb4ec993
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-04-14 13:33:39 -0300

    :sparkles: feat: create component Main Content

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <div className="content">
+        <h2>Olá, sou o Rafael Fortuna</h2>
+        <p>Desenvolvedor Frontend</p>
+        <p>Estou sempre aprendendo e buscando novos desafios.</p>
+      </div>
+    </div>
+  );
+}
+
+export default MainContent;
