27f3ff575757698fd02203dc8687cb9039ac1dd4
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-04-14 13:34:46 -0300

    :sparkles: feat: create component/section Skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section id="skills" className="section">
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React</li>
+        <li>Git e GitHub</li>
+        <li>Figma, UI/UX</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
