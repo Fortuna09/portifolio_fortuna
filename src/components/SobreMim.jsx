a76da78e7cfc0a53f138d89b98968d8fbce7022d
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-04-14 13:35:11 -0300

    :sparkles: feat: create component/section aboutme

---
+++ src/components/SobreMim.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SobreMim() {
+  return (
+    <section id="sobre-mim" className="section">
+      <h2>Sobre Mim</h2>
+      <p>Sou Rafael Fortuna, desenvolvedor frontend apaixonado por criar interfaces intuitivas e criativas.</p>
+    </section>
+  );
+}
+
+export default SobreMim;
