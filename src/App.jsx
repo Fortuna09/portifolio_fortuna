451cb963de849ea80e0af06df385c17dce771ae9
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-04-14 13:27:24 -0300

    :recycle: refactor: create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,23 @@
 import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import SobreMim from "./components/SobreMim";
+import Skills from "./components/Skills";
+import Projetos from "./components/Projetos";
+import Footer from "./components/Footer";
 import "./App.css";
 import "./reset.css";
 
 function App() {
   return (
     <div>
-      <div className="top-bar">
-        <div className="top-bar-content">
-          <img src="/R_logo.png" className="logo-girando" alt="logo" />
-          <h1>Rafael Fortuna</h1>
-        </div>
-
-        <div className="navigation">
-          <ul className="nav_selector">
-            <li>
-              <button>Projetos</button>
-            </li>
-            <li>
-              <button>Certificados</button>
-            </li>
-            <li>
-              <button>Contatos</button>
-            </li>
-          </ul>
-        </div>
-      </div> 
-
-      <div className="main-content">
-        <div className="content">
-          <h2>Olá, sou o Rafael Fortuna</h2>
-          <p>Desenvolvedor Frontend</p>
-          <p>Estou sempre aprendendo e buscando novos desafios.</p>
-        </div>
-
-      </div>
-
-      <div className="footer">
-
-      </div>
-
-      
+      <Header />
+      <MainContent />
+      <SobreMim />
+      <Skills />
+      <Projetos />
+      <Footer />
     </div>
-    
-
-
   );
 }
 
