1777af6f1636c30c0589768a9a6fd6cc4631f598
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-04-15 17:03:50 -0300

    feat(footer): add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Entre em contato:{" "}
+        <a href="fortunarafael9@gmail.com">fortunarafael9@gmail.com</a> |{" "}
+        <a href="https://github.com/Fortuna09" target="_blank" rel="noopener noreferrer">GitHub</a> |{" "}
+        <a href="https://www.linkedin.com/in/rafael-fortuna-990184264/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
