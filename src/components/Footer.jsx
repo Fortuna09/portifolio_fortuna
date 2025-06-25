1ec8c53f6616fb9bda6409d117f0f46b07177189
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-06-24 21:17:43 -0300

    v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
       <p>
         Entre em contato:{" "}
         <a href="fortunarafael9@gmail.com">
-          <img src="../../public/icon_email.png" alt="Email" />fortunarafael9@gmail.com</a> |{" "}
+          <img src="./icon_email.png" alt="Email" />fortunarafael9@gmail.com</a> |{" "}
 
         <a href="https://github.com/Fortuna09" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/icon_github.png" alt="GitHub" />GitHub</a> |{" "}
+          <img src="./icon_github.png" alt="GitHub" />GitHub</a> |{" "}
 
         <a href="https://www.linkedin.com/in/rafael-fortuna-990184264/" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/icon_linkedin.png" alt="LinkedIn" />LinkedIn</a>
+          <img src="./icon_linkedin.png" alt="LinkedIn" />LinkedIn</a>
       </p>
     </footer>
   );
