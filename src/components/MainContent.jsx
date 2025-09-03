484de1a2cb66a31be9af83f8b8cb61d23ed5ffbf
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2025-09-02 23:55:34 -0300

    new curriculo

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -14,7 +14,7 @@ function MainContent() {
         <div className="profile-info">
           <h1>Rafael Fortuna</h1>
           <p>Desenvolvedor Web</p>
-          <a href="/public/Rafael-Silva-Fortuna.pdf" download="Rafael-Silva-Fortuna.pdf">
+          <a href=".//Rafael-Silva-Fortuna.pdf" download="Rafael-Silva-Fortuna.pdf">
             Baixar meu Currículo
           </a>
         </div>
